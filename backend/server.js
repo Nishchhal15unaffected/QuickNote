32ffec1994982a3fd2501edfd2588169537ffa63
Nishchhal Prajapat <nishchhalprajapat@gmail.com>
2021-10-30 01:32:45 +0530

    finish quick note app

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 //deployment
 __dirname = path.resolve();
-if (process.env.NODE_ENV === "deployment") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/front-end/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"));
